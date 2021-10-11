f7bcd7ae34d23c7a57a8d97ae31c01da51cddee9
MartaGreen <iguana13131@gmail.com>
2021-10-11 19:29:38 +0400

    refactor: change structure of styles files; fix: font connection

--- src/imports/styles.imports.js
+++ src/imports/styles.imports.js
@@ -1,3 +1,5 @@
 import "./../kits/auth/login/login.scss";
 import "./../kits/auth/register/register.scss";
 import "./../kits/auth/auth.scss";
+import "./../kits/kits-styles.scss";
+import "./../index.scss";
