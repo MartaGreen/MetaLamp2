fe63fa8ff3476731c383843159ec4e7ec324108a
MartaGreen <iguana13131@gmail.com>
2021-10-15 00:06:07 +0400

    feat: add show dropdown func

--- src/kits/rooms/find-rooms/find-rooms.js
+++ src/kits/rooms/find-rooms/find-rooms.js
@@ -1,1 +1,7 @@
 import "./find-rooms.scss";
+
+const dropdownBtn = document.querySelector(".dropdown__btn");
+dropdownBtn.addEventListener("click", () => {
+  console.log(document.querySelector(".dropdown__content"));
+  document.querySelector(".dropdown__content").classList.toggle("-show-");
+});
