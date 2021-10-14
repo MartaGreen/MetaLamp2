4c8bd141e6244e509c3825d760d23f39305e6bf8
MartaGreen <iguana13131@gmail.com>
2021-10-14 13:27:55 +0400

    feat: add styles for calendar

--- src/kits/calendar/calendar.js
+++ src/kits/calendar/calendar.js
@@ -1,5 +1,20 @@
 import AirDatepicker from "air-datepicker";
 
+let applyBtn = {
+  content: "Применить",
+  class: "apply-btn",
+  onClick: () => {
+    console.log("применить");
+  },
+};
+
 new AirDatepicker("#choose-room", {
   inline: true,
+  range: true,
+
+  buttons: [applyBtn, "clear"],
+
+  onSelect(date) {
+    console.log("date", date);
+  },
 });
