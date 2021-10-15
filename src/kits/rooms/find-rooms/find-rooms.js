7652d8f3049d733ef9dd62e0cea53146d9b248a8
MartaGreen <iguana13131@gmail.com>
2021-10-15 12:52:47 +0400

    feat: add guests apply func

--- src/kits/rooms/find-rooms/find-rooms.js
+++ src/kits/rooms/find-rooms/find-rooms.js
@@ -1,14 +1,15 @@
 import "./find-rooms.scss";
 
+const guestsCounterField = document.getElementById("guests-counter_field");
+
 // Show dropdown content
-const dropdownBtn = document.querySelector(".dropdown__btn");
+const dropdownBtn = guestsCounterField.querySelector(".dropdown__btn");
 dropdownBtn.addEventListener("click", () => {
-  console.log(document.querySelector(".dropdown__content"));
-  document.querySelector(".dropdown__content").classList.toggle("-show-");
+  guestsCounterField.querySelector(".dropdown__content").classList.toggle("-show-");
 });
 
 // Change counter
-const dropdownContent = document.querySelector(".dropdown__content");
+const dropdownContent = guestsCounterField.querySelector(".dropdown__content");
 const dropdownItems = dropdownContent.querySelectorAll(".content__item");
 
 dropdownItems.forEach((item) => {
@@ -32,4 +33,42 @@ dropdownItems.forEach((item) => {
 
     rmBtn.classList.remove("-not-active-");
   });
+});
+
+// aply counter settings
+const applyBtn = guestsCounterField.querySelector(".apply");
+const cancelBtn = guestsCounterField.querySelector(".cancel");
+
+applyBtn.addEventListener("click", () => {
+  const valuesArr = Array.from(guestsCounterField.querySelectorAll(".counter__value"));
+  const values = valuesArr.map((val) => Number(val.innerHTML));
+  const guestsCount = values.reduce((prev, next) => prev + next);
+
+  let newName;
+  if (guestsCount == 0) {
+    cancelBtn.classList.remove("-show-");
+    newName = "Сколько гостей";
+  } else {
+    cancelBtn.classList.add("-show-");
+    if (guestsCount == 1) {
+      newName = `${guestsCount} гость`;
+    } else {
+      newName = `${guestsCount} гостей`;
+    }
+  }
+  dropdownBtn.querySelector(".btn__name").innerHTML = newName;
+  dropdownContent.classList.remove("-show-");
+
+  // dropdownBtn.querySelector(".btn__name").innerHTML =
+  //   guestsCount == 1
+  //     ? `${guestsCount} гость`
+  //     : guestsCount == 0
+  //     ? "Сколько гостей"
+  //     : `${guestsCount} гостей`;
+  // dropdownContent.classList.remove("-show-");
+});
+
+cancelBtn.addEventListener("click", () => {
+  const valuesArr = Array.from(guestsCounterField.querySelectorAll(".counter__value"));
+  valuesArr.forEach((val) => (val.innerHTML = 0));
 });
