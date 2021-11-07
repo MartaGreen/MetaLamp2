d1cdffd64b38a06a51b11a8c2305449ef5e81676
MartaGreen <iguana13131@gmail.com>
2021-11-07 13:39:00 +0400

    feat: add room reservation

--- src/kits/rooms/rooms.js
+++ src/kits/rooms/rooms.js
@@ -3,6 +3,7 @@ import "./cross-mixins/guests-counter/guests-counter.mixin";
 import "./cross-mixins/calendar/calendar.mixin";
 import "./find-rooms/find-rooms";
 import "./room-details/room-details";
+import "./room-reservation/room-reservation";
 
 // style files
 import "./cross-styles/dropdown.scss";
